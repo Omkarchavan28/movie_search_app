eec9eda0079b5623da8f9ce0792ef9e50dec5e85
Omkarchavan28 <chavanomkar245@gmail.com>
2019-10-07 15:49:48 +0530

    0710

--- app.js
+++ app.js
@@ -3,6 +3,46 @@ var app = express();
 var request = require('request');
 app.use(express.static('\public'));
 
+//database
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/uwatch';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+//
+//login
+app.post('/sign_up', function(req,res){ 
+    var name = req.body.name; 
+    var email =req.body.email; 
+    var pass = req.body.password; 
+    var phone =req.body.phone; 
+  
+    var data = { 
+        "name": name, 
+        "email":email, 
+        "password":pass, 
+        "phone":phone 
+    } 
+db.collection('details').insertOne(data,function(err, collection){ 
+        if (err) throw err; 
+        console.log("Record inserted Successfully"); 
+              
+    }); 
+          
+    return res.redirect('signup_success.ejs'); 
+}) 
+  
+
+
+
+
 var bodyParser =require("body-parser");
 
 app.set("view engine", "ejs");
@@ -12,6 +52,11 @@ app.get("/", function (req, res) {
     res.locals.title = "Uwatch";                    // THIS LINE IS KEY
     res.render('home');
 });
+app.get("/signup", function (req, res) {
+    res.locals.title = "Uwatch";                    // THIS LINE IS KEY
+    res.render('sign_up');
+});
+
 
 app.get("/results", function (req, res){
     var search_key = req.query.search;
